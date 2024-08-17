88fb887c50c5c1b9b1db665261ee083c78c8b6be
Aviroen <aviroen@gmail.com>
2024-08-17 09:55:52 -0700

    Add project files.

---
+++ KaraokeFramework.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewModdingAPI.Utilities;
+using StardewValley;
+
+namespace KaraokeFramework
+{
+    /// <summary>The mod entry point.</summary>
+    public class KaraokeFramework : Mod
+    {
+
+        /*********
+        ** Public methods
+        *********/
+        /// <summary>The mod entry point, called after the mod is first loaded.</summary>
+        /// <param name="helper">Provides simplified APIs for writing mods.</param>
+        public override void Entry(IModHelper helper)
+        {
+            //Register to events
+            helper.Events.GameLoop.UpdateTicking += this.OnUpdateTick;
+            helper.Events.Input.ButtonPressed += this.OnButtonPressed;
+            helper.Events.GameLoop.GameLaunched += this.OnGameLaunched;
+            helper.Events.GameLoop.Saving += this.OnSaving;
+            helper.Events.GameLoop.SaveLoaded += this.OnSaveLoaded;
+            helper.Events.Multiplayer.ModMessageReceived += this.OnModMessageReceived;
+            helper.Events.Multiplayer.PeerConnected += this.OnPeerConnected;
+
+            foreach (IContentPack contentPack in this.Helper.ContentPacks.GetOwned())
+            {
+                this.Monitor.Log($"Reading content pack: {contentPack.Manifest.Name} {contentPack.Manifest.Version} from {contentPack.DirectoryPath}");
+            }
+
+        }
+    }
+}
